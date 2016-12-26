8d35dd6f5d78e6f4b993807efcc5e04ed6dbc12f
Anton Shramko <antonshramko@yandex.ru>
2016-12-26 21:21:05 +0300

    initial shake, install libs, basic initial - cocoapods, babel with decorators, fs libs

---
+++ README.md
@@ -0,0 +1,6 @@
+## React Native Boilerplate
+My started react-native started boilerplate with basic libs after linking.
+
+- react-native: 0.39
+- realm: 0.15
+- cocoapods
---
+++ android/app/src/main/java/com/myreactnativeboilerplate/MainApplication.java
@@ -0,0 +1,50 @@
+package com.myreactnativeboilerplate;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import io.realm.react.RealmReactPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNFetchBlobPackage(),
+            new LinearGradientPackage(),
+            new RealmReactPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
